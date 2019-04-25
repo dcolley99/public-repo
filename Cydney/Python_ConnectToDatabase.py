4e7a3c339a19d6291e2eed3d1998b9cb5c818573
Derek Colley <derekc@eks.local>
2019-04-25 13:40:50 +0100

    250419

---
+++ Cydney/Python_ConnectToDatabase.py
@@ -0,0 +1,25 @@
+import pyodbc, string
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=localhost;'
+                      'Database=Cydney;'
+                      'Trusted_Connection=yes' )
+cursor = conn.cursor()
+cursor = conn.execute('SELECT name FROM sys.tables;')
+
+outfile = open('C:\\temp\\data.txt', 'w+') 
+
+for row in cursor:
+    row = str(row)
+    row = row.replace("'","") 
+    row = row.replace(",","")
+    row = row.replace("(","") 
+    row = row.replace(")","")
+    row = row.replace(" ","")
+    print(row)
+    outfile.write(row) 
+    outfile.write("\n")
+
+outfile.close() 
+
+
